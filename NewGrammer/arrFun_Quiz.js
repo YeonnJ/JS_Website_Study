3141907a98c7dcdc7a7adc00512e967fdaea39cb
YeonJae <duswo9595@naver.com>
2021-12-10 15:15:09 +0900

    Add:ArrFun Quiz 정답

--- NewGrammer/arrFun_Quiz.js
+++ NewGrammer/arrFun_Quiz.js
@@ -21,6 +21,7 @@ let capital = names.map((item) => {
 });
 
 // console.log(capital);
+// let upperCaseName = names.map((item)=>item.toUpperCase())
 
 let test = names.map((name) => {
   return name
@@ -32,10 +33,19 @@ let test = names.map((name) => {
 });
 
 // console.log(test);
+// let firstNames = names.map((item)=>item.split(' ')[0])
+
 let test2 = names.map((name) => {
   return name.split(" ")[0];
 });
 // console.log(test2);
+// let initialOnly = names.map((item) => {
+//   let splitName = item.split(" ");
+//   let initial = "";
+//   splitName.forEach((nameItem) => (initial += nameItem[0]));
+//   return initial;
+// })
+
 //-----------------------------------------------------
 // filter 문제
 // 이름에 a를 포함한 사람들을 출력하시오.
@@ -46,6 +56,7 @@ let person = names.filter((item) => {
   return item.includes("a"); //(조건)에 맞는 정보만 가져옴(결과값도 배열로 반환)
 });
 // console.log(person);
+// let includeA = names.filter((item)=>item.includes('a'))
 
 let people = names.filter((item, i) => {
   for (let i = 0; i < item.length; i++) {
@@ -55,6 +66,11 @@ let people = names.filter((item, i) => {
   }
 });
 // console.log(people);
+// let doubleLetter =  names.filter((item) => {
+//   let splitName = item.split("");
+//   return splitName.some((letter, index) => letter == splitName[index + 1]);
+// })
+
 //---------------------------------------------------
 //some 문제
 // 전체 이름의 길이가 20자 이상인 사람이 있는가?
@@ -64,11 +80,17 @@ let length = names.some((item) => {
   return item.length >= 20; //(조건)하나라도 있으면 true/false
 });
 // console.log(length);
+// console.log(names.some((item)=>item.length>=20))
 
 let nameP = names.map((name) => {
   return name.split(" ")[0].includes("p");
 });
 // console.log(nameP);
+// console.log(names.some((item)=>{
+//   let splitName = item.split(' ')
+//   splitName.pop()
+//   return splitName.some(eachName=>eachName.toLocaleLowerCase().includes("p"))
+// }))
 
 //------------------------------------------------------
 // every 문제
@@ -78,11 +100,13 @@ let every = names.every((item) => {
   return item.length >= 20;
 });
 // console.log(every);
+// console.log(names.every(item=>item.length>=20))
 
 let every2 = names.every((item) => {
   return item.includes("a");
 });
 // console.log(every2);
+// console.log(names.every(item=>item.includes('a')))
 //------------------------------------------------------
 // find 문제
 // 전체 이름의 길이가 20자 이상인 사람을 찾으시오.
@@ -91,12 +115,14 @@ let findName = names.find((item) => {
   return item.length >= 20;
 });
 // console.log(findName);
+// console.log(names.find(item=>item.length>=20))
 
 let middleName = names.filter((item) => {
   return item.split(" ").length === 3;
 });
 
 // console.log(middleName);
+// console.log(names.find(item=>item.split(' ').length>=3))
 //----------------------------------------------------------
 // findIndex 문제
 // 전체 이름의 길이가 20자 이상인 사람의 인덱스 번호를 찾으시오.
@@ -105,6 +131,7 @@ let nameLength = names.findIndex((item, index) => {
   return item.length >= 20;
 });
 // console.log(nameLength);
+// console.log(names.findIndex(item=>item.length>=20))
 
 let middleIndex = names.reduce((acc, cur, index) => {
   if (cur.split(" ").length === 3) {
@@ -112,4 +139,5 @@ let middleIndex = names.reduce((acc, cur, index) => {
   }
   return acc;
 }, []);
-console.log(middleIndex);
+// console.log(middleIndex);
+// console.log(names.findIndex(item=>item.split(' ').length>=3))
