f4c2f977b56ca8521de2a42b1a8f2a193013fdd1
YeonJae <duswo9595@naver.com>
2021-12-18 20:18:43 +0900

    Add:NewsPaper

--- API/main.js
+++ API/main.js
@@ -3,6 +3,8 @@ let menus = document.querySelectorAll(".menus button");
 menus.forEach((menu) =>
   menu.addEventListener("click", (event) => getNewsByTopic(event))
 );
+let searchButton = document.getElementById("search-button");
+
 const getLatestNews = async () => {
   let url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=12`
@@ -22,6 +24,27 @@ const getNewsByTopic = async (event) => {
   let topic = event.target.textContent.toLowerCase();
   let url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&page_size=12&topic=${topic}`
+  );
+  let header = new Headers({
+    "x-api-key": "LAjDLf7fgAhHe29KTbmQ-9sNhJanjXy8xZ_PLPR7xRw",
+  });
+  let response = await fetch(url, { headers: header });
+  let data = await response.json();
+  news = data.articles;
+  render();
+};
+
+const getNewsByKeyword = async () => {
+  //1. 검색키워드 읽어오기
+  //2. url에 검색키워드 붙이기
+  //3. 헤더준비
+  //4. url부르기
+  //5. 데이터가져오기
+  //6. 데이터보여주기
+
+  let keyword = document.getElementById("search-input").value;
+  let url = new URL(
+    `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=12`
   );
   let header = new Headers({
     "x-api-key": "LAjDLf7fgAhHe29KTbmQ-9sNhJanjXy8xZ_PLPR7xRw",
@@ -60,5 +83,5 @@ const render = () => {
 };
 //async, await ;비동기처리
 //API부르는 방식이 주로 ajax, axios, fetch가 있음. 이중에 fetch가 제일 간단함.
-
+searchButton.addEventListener("click", getNewsByKeyword);
 getLatestNews();
